848db60ee6d330bafcab0ea553e6f4014be09530
Jennifer Casto <CastoCustomCreations@gmail.com>
2024-09-06 14:09:54 -0400

    AD Banner

--- src/app/Components/global-banner/global-banner.component.ts
+++ src/app/Components/global-banner/global-banner.component.ts
@@ -8,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './global-banner.component.css'
 })
 export class GlobalBannerComponent {
+  slides: string[];
+  i: number;
 
+  constructor() {
+    this.i = 0;
+    this.slides = [
+      './bannerimages/ad1.png',
+      './bannerimages/ad2.png',
+      './bannerimages/ad3.png',
+      './bannerimages/ad4.png',
+    ];
+  }
+  getSlide() {
+    return this.slides[this.i];
+  }
+
+  getPrev() {
+    this.i == 0 ? (this.i = this.slides.length - 1) : this.i--;
+  }
+
+  getNext() {
+    this.i < this.slides.length - 1 ? this.i++ : (this.i = 0);
+  }
 }
+
